7117e550a34d0a90d426d62659fff275a0babecb
Fawwaz Khayyat <fawaz.khayat@gmail.com>
2019-04-09 15:25:32 -0400

    add routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+//import Home from './views/Home.vue'
+import Dashboard from '@/components/Dashboard'
+import NewEmployee from '@/components/NewEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+import EditEmployee from '@/components/EditEmployee'
 
 Vue.use(Router)
 
@@ -8,16 +12,22 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Dashboard',
+      component: Dashboard
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee
+    },
+    {
+      path: '/:employee_id',
+      name: 'view-employee',
+      component: ViewEmployee
+    },{
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
+      component: EditEmployee
     }
   ]
 })
